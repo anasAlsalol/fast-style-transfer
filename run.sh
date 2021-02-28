1ef58fc58a0e4553cf84f6c0c012002c71e53a02
Anas Salool <anasalsalool@gmail.com>
2021-02-28 10:25:15 +0200

    test

--- run.sh
+++ run.sh
@@ -14,7 +14,9 @@ git clone --branch v0.9.3 https://github.com/mozilla/DeepSpeech
 cd DeepSpeech
 pip3 install --upgrade pip==20.2.2 wheel==0.34.2 setuptools==49.6.0
 pip3 install --upgrade -e .
-pip3 install pathlib sox progressbar urllib3 -y
-python setup.py install
+#pip3 install pathlib sox progressbar urllib3 
 pip3 uninstall tensorflow -y
 pip3 install 'tensorflow-gpu==1.15.4'
+ls 
+pip3 install deepspeech-gpu
+python3 setup.py install
