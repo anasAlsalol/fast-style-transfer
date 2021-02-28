d7b2df0ee9b4c32e92d7713e750f512fb2c8856a
Anas Salool <anasalsalool@gmail.com>
2021-02-28 08:55:33 +0200

    edit bash for test

--- run.sh
+++ run.sh
@@ -9,10 +9,10 @@ apt-get update -y
 apt-get install sox libsox-fmt-mp3 virtualenv python3-dev wget git -y
 apt-get dist-upgrade -y
 virtualenv -p python3 $HOME/tmp/deepspeech-train-venv/
-. $HOME/tmp/deepspeech-train-venv/bin/activate
+source $HOME/tmp/deepspeech-train-venv/bin/activate
 
-git clone --branch v0.9.3 https://github.com/mozilla/DeepSpeech
-cd DeepSpeech
-pip3 install --upgrade pip==20.2.2 wheel==0.34.2 setuptools==49.6.0
-pip3 install --upgrade -e .
-python setup.py install
+#git clone --branch v0.9.3 https://github.com/mozilla/DeepSpeech
+#cd DeepSpeech
+#pip3 install --upgrade pip==20.2.2 wheel==0.34.2 setuptools==49.6.0
+#pip3 install --upgrade -e .
+#python setup.py install
