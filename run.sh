c279c36300c2696cd1273f3406f9c6791b1cbe2e
Anas Salool <anasalsalool@gmail.com>
2021-02-28 09:09:17 +0200

    edit bash for test

--- run.sh
+++ run.sh
@@ -14,7 +14,8 @@ virtualenv -p python3 venv/deepspeech-train-venv/
 source venv/deepspeech-train-venv/bin/activate
 
 git clone --branch v0.9.3 https://github.com/mozilla/DeepSpeech
-cd DeepSpeech
-pip3 install --upgrade pip==20.2.2 wheel==0.34.2 setuptools==49.6.0
-pip3 install --upgrade -e .
-python setup.py install
+ls /paperspace
+#cd DeepSpeech
+#pip3 install --upgrade pip==20.2.2 wheel==0.34.2 setuptools==49.6.0
+#pip3 install --upgrade -e .
+#python setup.py install
