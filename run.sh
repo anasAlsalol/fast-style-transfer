f571be48cea51ded4d814da08adfc851eb74e0e4
Anas Salool <anasalsalool@gmail.com>
2021-02-28 10:16:07 +0200

    test

--- run.sh
+++ run.sh
@@ -9,12 +9,12 @@ apt-get dist-upgrade -y
 mkdir venv
 mkdir venv/deepspeech-train-venv
 virtualenv -p python3.6 venv/deepspeech-train-venv/
-source venv/deepspeech-train-venv/bin/activate
+. venv/deepspeech-train-venv/bin/activate
 git clone --branch v0.9.3 https://github.com/mozilla/DeepSpeech
-cd /paperspace/DeepSpeech
+cd DeepSpeech
 pip3 install --upgrade pip==20.2.2 wheel==0.34.2 setuptools==49.6.0
 pip3 install --upgrade -e .
 pip3 install pathlib sox progressbar urllib3
 python setup.py install
-pip3 uninstall tensorflow
+pip3 uninstall tensorflow -y
 pip3 install 'tensorflow-gpu==1.15.4'
